38d885644f34d1fbc8bfa3125cc56d94d8559e64
gillyb <gillyb@gmail.com>
2013-04-30 21:18:53 +0300

    cleaning

--- src/Ultra.UI/Controllers/HomeController.cs
+++ src/Ultra.UI/Controllers/HomeController.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using MongoDB.Driver.Builders;
+﻿using System.Web.Mvc;
 using Ultra.Config.Routes;
-using Ultra.Controllers.Plumbing;
-using Ultra.Dal.Entities;
-using Ultra.Dal.Plumbing;
 using Ultra.Dal.Repositories;
-using Ultra.Services.JMeterOutput;
-using Ultra.Services.Jmx;
 
 namespace Ultra.Controllers
 {
