0b71dd512a6e68c73f1ebadda1f48d4b95b4c1b7
gillyb <gillyb@gmail.com>
2013-05-05 11:19:49 +0300

    removing batch file

--- src/Ultra.UI/jmx/RealWorld_unlimited.bat.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-set rampup=150
-set duration=1800
-
-echo rampup is %rampup%
-echo duration is %duration%
-START /B CMD /C CALL "D:\LoadTesting\apache-jmeter-2.9\bin\jmeter.bat" -Jhostname=uat.shopyourway.com -Joutputfile=output\mix_unlimited.csv -Jrampup=%rampup% -Jduration=%duration% -n -t "D:\LoadTesting\manual\scripts\Corsica\RealWorld_unlimited.jmx"
