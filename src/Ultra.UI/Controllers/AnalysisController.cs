097e9651c9baa509a525d90dbe68f5e4b9932ecf
gillyb <gillyb@gmail.com>
2013-05-12 18:40:40 +0300

    adding basic stuff for analysis view

---
+++ src/Ultra.UI/Controllers/AnalysisController.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using Ultra.Config.Routes;
+using Ultra.Services.JMeterOutput;
+using Ultra.Services.Jmx;
+
+namespace Ultra.Controllers
+{
+	public class AnalysisController : Controller
+	{
+		[Route("show-analysis/{jmeterOutputFile}")]
+		public ActionResult AnalyzeResults(string jmeterOutputFile)
+		{
+			var filepath = Path.Combine(JMeterOutputAnalyzer.JMeterOutputArchive, jmeterOutputFile);
+			var results = new JMeterOutputAnalyzer().Analyze(filepath, new JmxSettings {
+				Domain = "uat.shopyourway.com",
+				Duration = 1800,
+				RampUp = 150
+			});
+
+			return View(results);
+		}
+	}
+}
