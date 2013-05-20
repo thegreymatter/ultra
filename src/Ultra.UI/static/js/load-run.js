33e554942adc6155c6a361e2c7d6c35d4650a637
gillyb <gillyb@gmail.com>
2013-05-20 17:35:16 +0300

    nice delete from the load run history table

--- src/Ultra.UI/static/js/load-run.js
+++ src/Ultra.UI/static/js/load-run.js
@@ -21,6 +21,7 @@
 	});
 
 	$('#load-runs-history .delete').click(function() {
+		var loadRunRow = $(this).parents('.load-run');
 		var loadRunId = $(this).parents('tr').data('loadrunid');
 		if (confirm('Are you sure you want to delete this load run ?')) {
 			$.ajax({
@@ -28,7 +29,8 @@
 				type: 'POST',
 				data: { loadRunId: loadRunId },
 				success: function() {
-
+					loadRunRow.next('.load-run-details').remove();
+					loadRunRow.remove();
 				},
 				error: function() {
 
