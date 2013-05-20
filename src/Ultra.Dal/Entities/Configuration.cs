9b549a7fe9924706ff102214faac1019f8b6beac
gillyb <gillyb@gmail.com>
2013-05-20 16:41:54 +0300

    added a configuration entity

---
+++ src/Ultra.Dal/Entities/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using Ultra.Dal.Plumbing;
+
+namespace Ultra.Dal.Entities
+{
+	[Persistence(CollectionName = "configuration")]
+	public class UltraConfiguration : EntityBase
+	{
+		public string[] Hosts { get; set; }
+		public string JMeterBatFile { get; set; }
+		public string JmxScripts { get; set; }
+		public string JmxFileArchive { get; set; }
+		public string OutputArchive { get; set; }
+	}
+}
---
+++ src/Ultra.Dal/Repositories/ConfigurationRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Ultra.Dal.Entities;
+using Ultra.Dal.Plumbing;
+
+namespace Ultra.Dal.Repositories
+{
+	public interface IConfigurationRepository
+	{
+		UltraConfiguration GetConfiguration();
+		void SaveConfiguration(UltraConfiguration config);
+	}
+
+	public class ConfigurationRepository : IConfigurationRepository
+	{
+		// TODO: add some kind of caching level here...
+
+		private readonly IStorage<UltraConfiguration> _configStorage;
+
+		public ConfigurationRepository(IStorage<UltraConfiguration> configStorage)
+		{
+			_configStorage = configStorage;
+		}
+
+		public UltraConfiguration GetConfiguration()
+		{
+			return _configStorage.GetAll().First();
+		}
+
+		public void SaveConfiguration(UltraConfiguration config)
+		{
+			var currentConfig = _configStorage.GetAll().First();
+			config.Id = currentConfig.Id;
+			_configStorage.SaveOrUpdate(config);
+		}
+	}
+}
