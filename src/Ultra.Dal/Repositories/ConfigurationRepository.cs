6c0ec40cd9cf0f545fd857a60ee00dea38e94b7e
Shani Raba <rabashani@gmail.com>
2013-05-30 00:06:52 +0300

    handling NO configuration values (when starting the app - the configuration will be empty - the site should function properly)

--- src/Ultra.Dal/Repositories/ConfigurationRepository.cs
+++ src/Ultra.Dal/Repositories/ConfigurationRepository.cs
@@ -23,7 +23,7 @@ namespace Ultra.Dal.Repositories
 
 		public UltraConfiguration GetConfiguration()
 		{
-			return _configStorage.GetAll().FirstOrDefault();
+			return _configStorage.GetAll().FirstOrDefault() ?? new UltraConfiguration();
 		}
 
 		public void SaveConfiguration(UltraConfiguration config)
