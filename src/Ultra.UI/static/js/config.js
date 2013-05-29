8a0354fec001555a097c0f3b90cd119e173862d9
gillyb <gillyb@gmail.com>
2013-05-29 18:53:19 +0300

    js alerts upon saving configuration

--- src/Ultra.UI/static/js/config.js
+++ src/Ultra.UI/static/js/config.js
@@ -2,7 +2,13 @@
 
 	$('#save-config').click(function() {
 		var form = $('#edit-configuration-form');
-		$.post('/-/save-configuration', form.serialize());
+		$.post('/-/save-configuration', form.serialize())
+			.success(function() {
+				alert('Configuration saved successfully!');
+			})
+			.fail(function() {
+				alert('Error while saving configuration');
+			});
 	});
 
 });
